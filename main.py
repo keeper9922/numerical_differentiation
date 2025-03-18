30b58d6f208ddf0342c9fe1507cbd5b10f405d37
Andrey <elanser2005@gmail.com>
2025-03-18 23:55:21 +0300

    add file for functions and edited much of the code in main file: many changes in graph() function.

--- main.py
+++ main.py
@@ -1,45 +1,95 @@
 import matplotlib.pyplot as plt
+from funcs import *
 
 
-def graph(function_result: tuple[list[int | float],list[int | float],list[int | float],list[int | float],list[int | float]],
+
+def graph(function_result: tuple[list[int | float], list[int | float], list[int | float], list[int | float], list[int | float], list[int | float], list[int | float], list[int | float], list[int | float]],
             save_to_file: bool = False,
             result_folder: str = "result",
             result_name: str = "none"):
-  x, y1, y1_2, y2, y3 = function_result
-  # plt.plot(x, y, linestyle='--', marker='o', c="red", label=func_name)
-  plt.plot(x, y1, linestyle='--', linewidth=0.9, c="green", markersize=3, label="y1")
-  plt.plot(x, y1_2, linestyle='--', linewidth=0.7, c="red", markersize=2, label="y1_2")
-  plt.plot(x, y2, linestyle='--', linewidth=0.6, c="blue", markersize=1, label="y2")
-  plt.plot(x, y3, linestyle='--', linewidth=0.5, c="purple", markersize=1, marker="o", label="y3")
-  plt.xlabel('x')
-  plt.ylabel('f(x)')
-  plt.title(f'Численное дифференцирование')
+  """
+  Вывод всех посчитанных графиков для выбранных значений.
+  :param function_result: Результат в виде кортежа из 9 списков типа int или float. Первый список - сам x; 2, 3, 4 - аналитическое решение производных; 5, 6, 7, 8 - сами производные (где 6 - первая производная второго порядка точности)
+  :param save_to_file: Сохранять ли в файл построенные графики?
+  :param result_folder: Папка, куда сохранять построенные графики
+  :param result_name: Название файла
+  :return:
+  """
+  x, fx, analytic_fx1, analytic_fx2, analytic_fx3, fx1, fx1v2, fx2, fx3 = function_result
+  # Всего будет 8 графиков, столько же результатов функций мы и получаем в функции.
+  # Делаем холст с 3 графиками. Первый график - оригинальная функция.
+  # Второй график - Аналитическое решение
+  # Третий график - Производные по формулам из файла
+  # Сначала строим оригинальную функцию
+  plt.plot(x, fx, linestyle='solid', linewidth=2, c="black", label="y(x)")
+  plt.grid(True)  # включаем сетку, удобно
+  plt.ylabel('y(x)')  # обозначение оси ординат (y)
+  plt.xlabel('x')  # обозначение оси абсцисс (x)
+  plt.legend()
+  plt.title('Оригинальная функция', loc="left", pad=10)  # заголовок со смещением влево
+  # Первый график (аналитический)
+  plt.subplot(2, 1, 1)
+  # Теперь строим аналитическую первую производную
+  plt.plot(x, analytic_fx1, linestyle='solid', linewidth=1, c="red", label="y'(x)")
+  # Теперь строим аналитическую вторую производную
+  plt.plot(x, analytic_fx2, linestyle='solid', linewidth=1, c="green", label="y''(x)")
+  # Теперь строим аналитическую третью производную
+  plt.plot(x, analytic_fx3, linestyle='solid', linewidth=1, c="blue", label="y'''(x)")
+  plt.ylabel('y(x)')  # обозначение оси ординат (y)
+  plt.xlabel('x')  # обозначение оси абсцисс (x)
+  plt.title('Аналитическое решение', loc="left", pad=10)  # заголовок со смещением влево
   plt.grid(True)
   plt.legend()
-  if save_to_file:
-    plt.savefig(f"{result_folder}/{result_name}", dpi=300)
+  # Второй график (производные)
+  plt.subplot(2, 1, 2)
+  # Строим график первой производной
+  plt.plot(x, fx1, linestyle='solid', linewidth=1, c="red", label="f'(x)")
+  # Строим график первой производной второго порядка точности
+  plt.plot(x, fx1v2, linestyle='dashdot', linewidth=1, marker=".", c="#ff9628", label="f'(x) (второго порядка точности)")
+  # Строим график третьей производной
+  plt.plot(x, fx2, linestyle='solid', linewidth=1, c="green", label="f''(x)")
+  # Строим график четвёртой производной
+  plt.plot(x, fx3, linestyle='solid', linewidth=1, c="blue", label="f'''(x)")
+  plt.ylabel('y(x)')  # обозначение оси ординат (y)
+  plt.xlabel('x')  # обозначение оси абсцисс (x)
+  plt.title('Решение производных', loc="left", pad=10)  # заголовок со смещением влево
+  plt.grid(True)
+  plt.legend()
   plt.show()
+  if save_to_file:
+    plt.savefig(f"{result_folder}/{result_name}", dpi=1200)
 
-def f(x):
-    return (1 + x*x) / (2 * (1 + 2 * x*x)**1/2)
-    # return (x + 7) / 6 * (x*x + 2*x + 7)**1/2
-
-def f_diff(n: int, section: list, f):
+def f_diff(n: int, section: list):
     a = section[0]
     b = section[1]
     h = (b-a)/n
     x = []
-    for i in range(n):
+    for i in range(n+1):
         x.append(a+i*h)
-    y1 = []
-    y1_2 = []
-    y2 = []
-    y3 = []
-    for i in range(n):
-        y1.append( (f(x[i]+h) - f(x[i])) / h )
-        y1_2.append( (f(x[i]+h) - f(x[i]-h)) / 2*h )
-        y2.append( (f(x[i]+h) - 2*f(x[i]) + f(x[i]-h)) / h*h )
-        y3.append( (f(x[i]+2*h) - 2*f(x[i]+h) + 2*f(x[i]-h) - f(x[i]-2*h)) / 2*h*h*h )
-    return x, y1, y1_2, y2, y3
+    fx = []
+    analytic_fx1 = []
+    analytic_fx2 = []
+    analytic_fx3 = []
+    fx1 = []
+    fx1v2 = []
+    fx2 = []
+    fx3 = []
+    for i in range(n+1):
+        fx.append(y(x[i]))
+        analytic_fx1.append(a_y1(x[i]))
+        analytic_fx2.append(a_y2(x[i]))
+        analytic_fx3.append(a_y3(x[i]))
+        fx1.append(f1(x[i], h))
+        fx1v2.append(f1_v2(x[i], h))
+        fx2.append(f2(x[i], h))
+        fx3.append(f3(x[i], h))
+    print(x)
+    print(fx)
+    print(analytic_fx1)
+    print(analytic_fx2)
+    print(analytic_fx3)
+    return x, fx, analytic_fx1, analytic_fx2, analytic_fx3, fx1, fx1v2, fx2, fx3
 
-graph(f_diff(10000, [1, 10000], f))
+graph(
+f_diff(20, [-10, 10])
+)
