d04f8007e8a52281666d2848acff03e2b2c3fec1
Andrey <elanser2005@gmail.com>
2025-03-19 15:06:00 +0300

    many changes. Only showing approx graphics for each num. diff.

--- main.py
+++ main.py
@@ -1,73 +1,32 @@
 import matplotlib.pyplot as plt
 from funcs import *
 
-
-
-def graph(function_result: tuple[list[float | int], list[float | int], list[float | int], list[float | int], list[float | int]],
-            save_to_file: bool = False,
-            result_category: str = "result",
-            result_name: str = "none"):
-  """
-  Вывод всех посчитанных графиков для выбранных значений.
-  :param function_result: Результат в виде кортежа из 9 списков типа int или float. Первый список - сам x; 2, 3, 4 - аналитическое решение производных; 5, 6, 7, 8 - сами производные (где 6 - первая производная второго порядка точности)
-  :param save_to_file: Сохранять ли в файл построенные графики?
-  :param result_category: Как мы объединяем графики для разных N
-  :param result_name: Название файла
-  :return:
-  """
-  x, fx, fx_left, fx_right, fx_center = function_result
-
-  plt.subplot(2, 1, 1)
-  plt.plot(x, fx)
-  plt.subplot(2, 1, 2)
-  plt.plot(x, fx_left, label="left")
-  plt.plot(x, fx_right, label="right")
-  plt.plot(x, fx_center, label="center")
-  plt.legend()
-
-  if save_to_file:
-    plt.savefig(f"result/{result_category}_{result_name}", dpi=1200)
-    plt.close()
-  else:
-    plt.show()
-
-def f_diff(divisions: int, section: list):
-    a = section[0]
-    b = section[1]
-    h = (b-a)/divisions
+ab = [-10, 10]
+N = 50
+for i in (1, 2, 4):
+    h = (ab[1]-ab[0])/i*N
     x = []
-    for i in range(divisions+1):
-        x.append(a+i*h)
-    fx = []
-    delta_left = []
-    delta_right = []
-    delta_center = []
-    for i in range(divisions+1):
-        fx.append(y(x[i]))
-        delta_left.append((f1_left(x[i], h) - y(x[i])) / f1_left(x[i], h))
-        delta_right.append((f1_right(x[i], h) - y(x[i])) / f1_right(x[i], h))
-        delta_center.append((f1_center(x[i], h) - y(x[i])) / f1_center(x[i], h))
-    # print(x)
-    # print(fx)
-    # print(analytic_fx1)
-    # print(analytic_fx2)
-    # print(analytic_fx3)
-    return x, fx, delta_left, delta_right, delta_center
-
-sec = [-10, 10]
-n = 50 # abs(sec[0]) + abs(sec[1])
-graph(
-    f_diff(n, sec)
-)
-graph(
-    f_diff(2*n, sec)
-)
-graph(
-    f_diff(4*n, sec)
-)
-# graph(
-#     f_diff(2*10000, sec)
-# )
-# graph(
-#     f_diff(4*10000, sec)
-# )
+    for j in range(i*N+1):
+        x.append(ab[0]+j*h)
+    delta1_left = []
+    delta1_right = []
+    delta1_center = []
+    delta2 = []
+    delta3 = []
+    for j in range(i*N+1):
+        delta1_left.append((f1_left(x[j], h) - y(x[j])) / f1_left(x[j], h))
+        delta1_right.append((f1_right(x[j], h) - y(x[j])) / f1_right(x[j], h))
+        delta1_center.append((f1_center(x[j], h) - y(x[j])) / f1_center(x[j], h))
+        delta2.append((f2(x[j], h) - y(x[j])) / f2(x[j], h))
+        delta3.append((f3(x[j], h) - y(x[j])) / f3(x[j], h))
+    plt.subplot(3, 1, 1)
+    plt.title(f"N = {i*N}")
+    plt.plot(x, delta1_left, label='left')
+    plt.plot(x, delta1_right, label='right')
+    plt.plot(x, delta1_center, label='center')
+    plt.subplot(3, 1, 2)
+    plt.plot(x, delta2)
+    plt.subplot(3, 1, 3)
+    plt.plot(x, delta3)
+    plt.show()
+    plt.close()
